d74793c2af2dbea92103db9dcc9ac325f02f1338
BohdanRadchuk <shpionuse@gmail.com>
2018-08-18 16:06:11 +0300

    db rework

--- src/main/resources/db/watch_repair.sql
+++ src/main/resources/db/watch_repair.sql
@@ -1,58 +1,50 @@
-CREATE DATABASE IF NOT EXISTS watch_repair 
+CREATE DATABASE IF NOT EXISTS watch_repair
   CHARACTER SET utf8
   COLLATE utf8_general_ci;
 
 USE watch_repair;
 
 CREATE TABLE `user` (
-`id_user` INT NOT NULL AUTO_INCREMENT,
-`name` VARCHAR(64) NOT NULL,
-`surname` VARCHAR(64) NOT NULL,
-`email` VARCHAR(64) UNIQUE NOT NULL,
-`password` VARCHAR(64) NOT NULL,
-`role` ENUM('USER','MANAGER','MASTER','ADMIN') NOT NULL,
-PRIMARY KEY (`id_user`)
+  `id_user` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(64) NOT NULL,
+  `surname` VARCHAR(64) NOT NULL,
+  `email` VARCHAR(64) UNIQUE NOT NULL,
+  `password` VARCHAR(64) NOT NULL,
+  `role` ENUM('USER','MANAGER','MASTER','ADMIN') NOT NULL,
+  PRIMARY KEY (`id_user`)
 );
 
 CREATE TABLE `service`(
-`id_service` INT NOT NULL AUTO_INCREMENT,
-`type` VARCHAR(256) NOT NULL,
-`price` INT,
-PRIMARY KEY (`id_service`)
+  `id_service` INT NOT NULL AUTO_INCREMENT,
+  `type_en` VARCHAR(256) NOT NULL,
+  `type_ua` varchar(256) NOT NULL,
+  `price` INT NOT NULL,
+  PRIMARY KEY (`id_service`)
 );
 
 CREATE TABLE `request`(
-`id_request` INT NOT NULL AUTO_INCREMENT,
-`id_service` INT NOT NULL,
-`price` DECIMAL,
-FOREIGN KEY (`id_service`) REFERENCES `service`(`id_service`) ON DELETE RESTRICT ON UPDATE CASCADE,
-PRIMARY KEY (`id_request`)
-);
-
-CREATE TABLE `status`(
-`id_status` INT NOT NULL AUTO_INCREMENT,
-`status` ENUM ('UNCHECKED', 'REJECTED', 'CONFIRMED', 'IN_WORK', 'DONE') NOT NULL,
-`date` DATETIME NOT NULL,
-`id_request` INT NOT NULL,
-`id_user` INT NOT NULL,
-FOREIGN KEY (`id_request`) REFERENCES `request`(`id_request`) ON DELETE RESTRICT ON UPDATE CASCADE,
-FOREIGN KEY (`id_user`) REFERENCES `user`(`id_user`) ON DELETE RESTRICT ON UPDATE CASCADE,
-PRIMARY KEY (`id_status`)
+  `id_request` INT NOT NULL AUTO_INCREMENT,
+  `id_user` INT NOT NULL,
+  `id_service` INT NOT NULL,
+  `status` ENUM ('UNCHECKED', 'REJECTED', 'CONFIRMED', 'IN_WORK', 'DONE'),
+  `price` INT,
+  `id_manager` INT,
+  `consideration_date` DATETIME,
+  `id_master` INT,
+  `in_work_date` DATETIME,
+  `done_date` DATETIME,
+  CONSTRAINT FOREIGN KEY (`id_user`) REFERENCES `user`(`id_user`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`id_manager`) REFERENCES `user`(`id_user`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`id_master`) REFERENCES `user`(`id_user`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`id_service`) REFERENCES `service`(`id_service`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (`id_request`)
 );
 
 CREATE TABLE `comment`(
-`id_comment` INT NOT NULL AUTO_INCREMENT,
-`id_user` INT NOT NULL,
-`commentary` VARCHAR (1000),
-FOREIGN KEY (`id_user`) REFERENCES `user`(`id_user`) ON DELETE RESTRICT ON UPDATE CASCADE,
-PRIMARY KEY (`id_comment`)
+  `commentary` VARCHAR (1000) NOT NULL,
+  `id_request` INT NOT NULL,
+  CONSTRAINT FOREIGN KEY (`id_request`) REFERENCES `request` (`id_request`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (`id_request`)
 );
 
-CREATE TABLE `comment_request`(
-`id_comment` INT,
-`id_request` INT,
-FOREIGN KEY (`id_comment`) REFERENCES `comment`(`id_comment`) ON DELETE RESTRICT ON UPDATE CASCADE,
-FOREIGN KEY (`id_request`) REFERENCES `request` (`id_request`) ON DELETE RESTRICT ON UPDATE CASCADE,
-PRIMARY KEY (`id_comment`, `id_request`)
-);
 
